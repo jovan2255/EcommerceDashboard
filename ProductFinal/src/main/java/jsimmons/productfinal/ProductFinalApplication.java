6317be7e1d76086a33ef49e002e4f9103a39f341
jovan2255 <43731992+jovan2255@users.noreply.github.com>
2019-12-11 10:07:17 -0500

    Add files via upload

---
+++ ProductFinal/src/main/java/jsimmons/productfinal/ProductFinalApplication.java
@@ -0,0 +1,13 @@
+package jsimmons.productfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductFinalApplication.class, args);
+	}
+
+}
